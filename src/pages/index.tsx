5c036d3c869cfc0f6432ee87c984862bce991db8
Jadiê <jadieoliveira00@gmail.com>
2021-05-05 21:33:49 -0300

    criando componente básico de mapa com leaflet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
-import Main from 'components/Main'
+import dynamic from 'next/dynamic'
+
+const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Main />
+  return (
+    <Map
+      places={[
+        {
+          id: '1',
+          name: 'Fortaleza',
+          slug: 'fortaleza',
+          location: {
+            latitude: 9,
+            longitude: -4
+          }
+        }
+      ]}
+    />
+  )
 }
