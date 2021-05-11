eea7e3bc20fa6791c75889d5239866e85e2d7272
Jadiê <jadieoliveira00@gmail.com>
2021-05-11 19:38:36 -0300

    gerando types para o graphql e paginas din

---
+++ src/template/Pages/index.tsx
@@ -0,0 +1,24 @@
+import LinkWrapper from 'components/LinkWrapper'
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+import * as S from './styles'
+
+export type PageTemplateProps = {
+  heading: string
+  body: string
+}
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} />
+    </LinkWrapper>
+
+    <S.Heading>{heading}</S.Heading>
+
+    <S.Body>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
+    </S.Body>
+  </S.Content>
+)
+
+export default PageTemplate
